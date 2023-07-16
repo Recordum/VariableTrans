99b62da84bf3513a1a40c7c884c769cc0e117378
Recordum <mingyu.oh15b@gmail.com>
2023-07-17 07:58:15 +0900

    [Setting] Translation module 생성

--- variable-trans-backend/src/app.module.ts
+++ variable-trans-backend/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TranslationService } from './translation/translation.service';
+import { TranslationController } from './translation/translation.controller';
+import { TranslationModule } from './translation/translation.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TranslationModule],
+  controllers: [AppController, TranslationController],
+  providers: [AppService, TranslationService],
 })
 export class AppModule {}
