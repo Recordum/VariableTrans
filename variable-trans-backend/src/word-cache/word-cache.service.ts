7d8d07bbc78ae4dee81e09790bdacfb10b2935cb
Recordum <mingyu.oh15b@gmail.com>
2023-07-18 04:59:08 +0900

    [Feat] WordCachedServi interface isWordcached 추가

--- variable-trans-backend/src/word-cache/word-cache.service.ts
+++ variable-trans-backend/src/word-cache/word-cache.service.ts
@@ -2,4 +2,5 @@ export interface WordCacheService {
   setWordCached(koreanWord: string, variable: string): void;
   deleteWordCached(koreanWord: string): void;
   getCachedVariable(koreanWord: string): string;
+  isWordCached(koreanWord: string): boolean;
 }
