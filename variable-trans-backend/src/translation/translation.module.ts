8ac8f2f23e87d5020515ba0173e40a19ed35fe82
Recordum <mingyu.oh15b@gmail.com>
2023-07-18 05:47:04 +0900

    [Setting] translationModule 의존관계 설정

--- variable-trans-backend/src/translation/translation.module.ts
+++ variable-trans-backend/src/translation/translation.module.ts
@@ -1,4 +1,22 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
+import { WordCacheModule } from 'src/word-cache/word-cache.module';
+import { RequestTranslationImpl } from './request-translation/implementation/request-translation.impl';
+import { TranslationController } from './translation.controller';
+import { TranslationServiceImpl } from './implementation/translation.service.impl';
 
-@Module({})
+@Module({
+  imports: [HttpModule, WordCacheModule],
+  providers: [
+    {
+      provide: 'RequestTranslation',
+      useClass: RequestTranslationImpl,
+    },
+    {
+      provide: 'TranslationService',
+      useClass: TranslationServiceImpl,
+    },
+  ],
+  controllers: [TranslationController],
+})
 export class TranslationModule {}
