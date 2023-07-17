b45b8f07a36b3c8a510a5cca30638ab59e372ca8
Recordum <mingyu.oh15b@gmail.com>
2023-07-18 04:37:44 +0900

    [Test] WordCacheMapService Test 코드 추가

---
+++ variable-trans-backend/src/word-cache/implementation/word-cache-map.spec.ts
@@ -0,0 +1,16 @@
+import { WordCacheMapService } from './word-cache-map.service';
+
+describe('WordCacheMapService', () => {
+  const service: WordCacheMapService = new WordCacheMapService();
+
+  it('should set and get cached word', () => {
+    service.setWordCached('안녕', 'hello');
+    expect(service.getCachedVariable('안녕')).toBe('hello');
+  });
+
+  it('should delete cached word', () => {
+    service.setWordCached('안녕', 'hello');
+    service.deleteWordCached('안녕');
+    expect(() => service.getCachedVariable('안녕')).toThrowError(Error);
+  });
+});
