3937163f74424d8ebd35f1d87c467d6a77e99686
Recordum <mingyu.oh15b@gmail.com>
2023-07-23 23:59:28 +0900

    [Test] TranslationService: transalteVariable 테스트 코드 추가

---
+++ variable-trans-backend/src/translation/translation.service.spec.ts
@@ -0,0 +1,59 @@
+import { WordCacheService } from 'src/word-cache/word-cache.service';
+import { TranslationServiceImpl } from './implementation/translation.service.impl';
+import { RequestTranslation } from './request-translation/request-translation';
+import { Test, TestingModule } from '@nestjs/testing';
+
+describe('TranslationService', () => {
+  let service: TranslationServiceImpl;
+  let requestTranslation: jest.Mocked<RequestTranslation>;
+  let wordCacheService: jest.Mocked<WordCacheService>;
+
+  beforeEach(async () => {
+    requestTranslation = {
+      translateVariable: jest.fn(),
+      recommandVariable: jest.fn(),
+    };
+
+    wordCacheService = {
+      isWordCached: jest.fn(),
+      getCachedVariable: jest.fn(),
+      setWordCached: jest.fn(),
+      deleteWordCached: jest.fn(),
+    };
+
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        TranslationServiceImpl,
+        {
+          provide: 'RequestTranslation',
+          useValue: requestTranslation,
+        },
+        {
+          provide: 'WordCacheService',
+          useValue: wordCacheService,
+        },
+      ],
+    }).compile();
+
+    service = module.get<TranslationServiceImpl>(TranslationServiceImpl);
+  });
+  describe('translationVariable', () => {
+    it('Cache 에 존재하는 단어이면 변수명을 Cache에서 return', async () => {
+      wordCacheService.isWordCached.mockReturnValue(true);
+      wordCacheService.getCachedVariable.mockReturnValue('validateUserId');
+
+      const result = await service.translateVariable(
+        '사용자ID를 유효성 검사하다',
+        'test-user',
+      );
+
+      expect(result).toBe('validateUserId');
+      expect(wordCacheService.isWordCached).toHaveBeenCalledWith(
+        '사용자ID를 유효성 검사하다',
+      );
+      expect(wordCacheService.getCachedVariable).toHaveBeenCalledWith(
+        '사용자ID를 유효성 검사하다',
+      );
+    });
+  });
+});
