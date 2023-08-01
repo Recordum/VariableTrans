d9bfc51a133b4397b90afc6a2c20a05dfc0c6af4
Recordum <mingyu.oh15b@gmail.com>
2023-08-01 15:06:54 +0900

    [Test] WordCacheMap 테스트 설명 수정

--- variable-trans-backend/src/word-cache/implementation/word-cache-map.spec.ts
+++ variable-trans-backend/src/word-cache/implementation/word-cache-map.spec.ts
@@ -11,18 +11,18 @@ describe('WordCacheMapService', () => {
     service = module.get<WordCacheMapService>(WordCacheMapService);
   });
 
-  it('should set and get cached word', () => {
+  it('setWordCached: 단어를 캐시 Map 에 저장', () => {
     service.setWordCached('안녕', 'hello');
     expect(service.getCachedVariable('안녕')).toBe('hello');
   });
 
-  it('should delete cached word', () => {
+  it('deletWordCached : 캐시 Map 에서 단어 삭제, 없는 단어를 캐시에서 찾을시 Error 발생', () => {
     service.setWordCached('안녕', 'hello');
     service.deleteWordCached('안녕');
     expect(() => service.getCachedVariable('안녕')).toThrowError(Error);
   });
 
-  it('should return true exist cached word', () => {
+  it('IsWordCached : 캐시되어있는 단어에 true 반환', () => {
     service.setWordCached('안녕', 'hello');
     expect(service.isWordCached('안녕')).toBe(true);
   });
