8306f37af959bc525aa527b3c7f756ee1d053d77
Recordum <mingyu.oh15b@gmail.com>
2023-08-06 02:11:42 +0900

    [Feat] UserService: login 기능 추가

--- variable-trans-backend/src/user/user.service.ts
+++ variable-trans-backend/src/user/user.service.ts
@@ -4,10 +4,14 @@ import { UserRepository } from './user.repository';
 import { User } from './entity/user.entity';
 import { LoginUserDto } from './dto/login-user.dto';
 import * as crypto from 'crypto';
+import { SessionService } from './session/session.service';
 
 @Injectable()
 export class UserService {
-  constructor(private readonly userRepository: UserRepository) {}
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly sessionService: SessionService,
+  ) {}
 
   public async registerUser(registerUserDto: RegisterUserDto) {
     await registerUserDto.encodePassword();
@@ -23,5 +27,25 @@ export class UserService {
       return true;
     }
     return false;
+  }
+
+  public async login(loginUserDto: LoginUserDto): Promise<string> {
+    const user: User = await this.userRepository.findUserByEmail(
+      loginUserDto.getUserEmail(),
+    );
+    if (
+      !user ||
+      !this.validatePassword(loginUserDto.getPassword(), user.passowrd)
+    ) {
+      throw new UnauthorizedException('잘못된 Email 혹은 비밀번호 입니다.');
+    }
+
+    const sessionId = crypto.randomBytes(32).toString('hex');
+    this.sessionService.setSessionData(sessionId, user);
+    return sessionId;
+  }
+
+  private validatePassword(loginPassword: string, password: string): boolean {
+    return loginPassword === password;
   }
 }
