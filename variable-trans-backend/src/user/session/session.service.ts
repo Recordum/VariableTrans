d3adf1826b3e8ec82c86f387d8110164d8c9716b
Recordum <mingyu.oh15b@gmail.com>
2023-08-07 15:15:55 +0900

    [Feat] SCessionService interface 추가

--- variable-trans-backend/src/user/session/session.service.ts
+++ variable-trans-backend/src/user/session/session.service.ts
@@ -1,7 +1,7 @@
 import { User } from '../entity/user.entity';
 
 export interface SessionService {
-  getSessionData(sessionId: string): string;
-  setSessionData(sessionId: string, user: User): void;
-  deleteSessionData(): void;
+  getSessionData(sessionId: string): Promise<string>;
+  setSessionData(sessionId: string, user: User): Promise<void>;
+  deleteSessionData(): Promise<void>;
 }
