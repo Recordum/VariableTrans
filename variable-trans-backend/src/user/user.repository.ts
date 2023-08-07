882b5ad536710899a2c311e2c394315f7d117869
Recordum <mingyu.oh15b@gmail.com>
2023-08-08 03:50:49 +0900

    [Modify] UserRpositor interface  findUserById 추가:

--- variable-trans-backend/src/user/user.repository.ts
+++ variable-trans-backend/src/user/user.repository.ts
@@ -1,9 +1,9 @@
-import { RegisterUserDto } from './dto/register-user.dto';
 import { User } from './entity/user.entity';
 
 export interface UserRepository {
   save(user: User): void;
   findUserByEmail(userEmail: string): Promise<User>;
   findPasswordByEmail(id: string): Promise<User>;
+  findUserById(id: string): Promise<User>;
   setPremiumUser(id: string): void;
 }
