6331bdc564105cb1bc02635fd1b46593e226fe5c
Recordum <mingyu.oh15b@gmail.com>
2023-08-11 15:13:23 +0900

    [Fix] RedisSessionService: getSessionData() sesiondata를 새로운 dto를 변환하는 로직 추가

--- variable-trans-backend/src/user/session/implementation/redis-sessoin.service.ts
+++ variable-trans-backend/src/user/session/implementation/redis-sessoin.service.ts
@@ -1,15 +1,25 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { SetSessionDto } from '../dto/set-session.dto';
+import { SetSessionDto, SetSessionDtoBuilder } from '../dto/set-session.dto';
 import { SessionService } from '../session.service';
 import { CACHE_MANAGER } from '@nestjs/cache-manager';
 import { Cache } from 'cache-manager';
+import { plainToClass } from 'class-transformer';
 
 @Injectable()
 export class RedisSessionService implements SessionService {
   constructor(@Inject(CACHE_MANAGER) private redis: Cache) {}
 
   public async getSessionData(sessionId: string): Promise<SetSessionDto> {
-    return await this.redis.get(sessionId);
+    const sessionData = await this.redis.get(sessionId);
+    if (!sessionData) {
+      throw new Error('세션 저장소에 세션이 존재하지 않습니다.');
+    }
+    return new SetSessionDtoBuilder()
+      .setSessionId(sessionData['sessionId'])
+      .setUserId(sessionData['userId'])
+      .setGrade(sessionData['grade'])
+      .setRequestLimit(sessionData['requestLimit'])
+      .build();
   }
   public async setSessionData(setSessionDto: SetSessionDto): Promise<void> {
     await this.redis.set(setSessionDto.getSessionId(), setSessionDto, 86400);
