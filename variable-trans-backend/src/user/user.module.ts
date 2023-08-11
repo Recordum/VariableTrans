26d25b34731158343a5d3726bbbb64db997329f7
Recordum <mingyu.oh15b@gmail.com>
2023-08-11 12:11:43 +0900

    [Setting] UserModule: Redis cache module 추가

--- variable-trans-backend/src/user/user.module.ts
+++ variable-trans-backend/src/user/user.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
-import { UserService } from './user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserService } from './user.service';
+import { UserController } from './user.controller';
 import { User } from './entity/user.entity';
+import { CacheModule } from '@nestjs/cache-manager';
+import * as redisStore from 'cache-manager-ioredis';
+import { RedisSessionService } from './session/implementation/redis-sessoin.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
-  providers: [UserService],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    CacheModule.register({
+      store: redisStore,
+      host: 'localhost',
+      port: 6379,
+    }),
+  ],
+  providers: [
+    UserService,
+    {
+      provide: 'SessionService',
+      useClass: RedisSessionService,
+    },
+  ],
+  controllers: [UserController],
 })
 export class UserModule {}
