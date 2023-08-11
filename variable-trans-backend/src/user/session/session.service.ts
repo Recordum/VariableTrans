43fa8e1814ce85948aa25cbad9e0dc5bdc1f0173
Recordum <mingyu.oh15b@gmail.com>
2023-08-11 12:09:36 +0900

    [Feat] SessionService interface 추가

--- variable-trans-backend/src/user/session/session.service.ts
+++ variable-trans-backend/src/user/session/session.service.ts
@@ -1,7 +1,7 @@
 import { SetSessionDto } from './dto/set-session.dto';
 
 export interface SessionService {
-  getSessionData(sessionId: string): Promise<string>;
+  getSessionData(sessionId: string): Promise<SetSessionDto>;
   setSessionData(setSessionDto: SetSessionDto): Promise<void>;
-  deleteSessionData(): Promise<void>;
+  deleteSessionData(sessionId: string): Promise<void>;
 }
