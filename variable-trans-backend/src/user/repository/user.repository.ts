425f1474f452fb7246f2bccd28f6915e71e3d946
Recordum <mingyu.oh15b@gmail.com>
2023-08-14 18:23:10 +0900

    [Modify] UserRepository interface updaeteRequesstLit, updatePassword 추가

--- variable-trans-backend/src/user/repository/user.repository.ts
+++ variable-trans-backend/src/user/repository/user.repository.ts
@@ -4,4 +4,6 @@ export interface UserRepository {
   saveUser(user: User): Promise<void>;
   findUserByEmail(userEmail: string): Promise<User>;
   findUserById(id: string): Promise<User>;
+  updateRequestLimit(id: string, requestLimit: number): Promise<void>;
+  updatePassword(id: string, password: string): Promise<void>;
 }
