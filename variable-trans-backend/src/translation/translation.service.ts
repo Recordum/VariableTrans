e7c2793163353a36af1eff89e55833a7bbf9e8c7
Recordum <mingyu.oh15b@gmail.com>
2023-08-15 00:02:04 +0900

    [Refactor] TranslatonService: translateVariable 구현

--- variable-trans-backend/src/translation/translation.service.ts
+++ variable-trans-backend/src/translation/translation.service.ts
@@ -1,40 +1,24 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { Translator } from './request-translation/translator';
-import { WordCacheService } from 'src/word-cache/word-cache.service';
+import { Translator } from './translator/translator';
 
 @Injectable()
 export class TranslationService {
   constructor(
     @Inject('Translator')
     private readonly translator: Translator,
-    @Inject('WordCacheService')
-    private readonly wordCacheService: WordCacheService,
+    @Inject('WordService')
+    private readonly wordService: WordService,
   ) {}
 
-  public async translateVariable(
-    koreanVariable: string,
-    userId: string,
-  ): Promise<string> {
-    // if (this.requestUsage.isOverUsage(userId)) {
-    //   return '일일 사용량이 초과 되었습니다';
-    // }
-    if (this.wordCacheService.isWordCached(koreanVariable)) {
-      console.log('cached');
-      //word 요청 횟수 count 추가하는 메소드
-      return this.wordCacheService.getCachedVariable(koreanVariable);
+  public async translateVariable(korean: string): Promise<string> {
+    let word: string = await this.wordService.findWord(korean);
+    if (word) {
+      return word;
     }
-    //MongoDB 연결
-    // const variable = await this.wordService.findWord(koreanVariable);
 
-    // if (variable !== undefined) {
-    //   this.wordCacheService.setWordCached(koreanVariable, variable);
-    //   return variable;
-    // }
-
-    const variable = await this.translator.translateVariable(koreanVariable);
-
-    this.wordCacheService.setWordCached(koreanVariable, variable);
-    return variable;
+    word = await this.translator.translateVariable(korean);
+    await this.wordService.saveWord(korean, word);
+    return word;
   }
 
   public recommandVariable(contents: string, userId: string): Promise<string> {
