af3063a790f3d96f2828ecc3bc863c4a3cbad5cc
Recordum <mingyu.oh15b@gmail.com>
2023-08-14 22:27:21 +0900

    [Refactor] WordService 테스트 WordRepostiory를 class 구현 에서 mock 객체로 변경

--- variable-trans-backend/src/word/word.service.spec.ts
+++ variable-trans-backend/src/word/word.service.spec.ts
@@ -1,33 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { CacheWordService } from './cache-word/cache-word.service';
 import { WordService } from './word.service';
 import { CacheMapWordService } from './cache-word/implementation/cache-map-word.service';
 
-export class MockWordRepository implements WordRepository {
-  findWordByKorean(korean: string): Promise<string> {
-    throw new Error('Method not implemented.');
-  }
-  saveWord(word: string, korean: string): Promise<void> {
-    throw new Error('Method not implemented.');
-  }
-}
 describe('WordService', () => {
   let service: WordService;
-  let wordRepository: MockWordRepository;
-  let cacheWordService: CacheWordService;
-  let KOREAN;
-  let WORD;
+  let wordRepository: jest.Mocked<WordRepository>;
+  let cacheWordService: CacheMapWordService;
+  let KOREAN: string;
+  let WORD: string;
+
   beforeEach(async () => {
+    wordRepository = {
+      findWordByKorean: jest.fn(),
+      saveWord: jest.fn(),
+    };
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         WordService,
-        { provide: 'WordRepository', useClass: MockWordRepository },
-        { provide: 'CacheWordSerivce', useClass: CacheMapWordService },
+        { provide: 'CacheWordService', useClass: CacheMapWordService },
+        { provide: 'WordRepository', useValue: wordRepository },
       ],
     }).compile();
 
     service = module.get<WordService>(WordService);
-    wordRepository = module.get<MockWordRepository>('WordRepository');
     cacheWordService = module.get<CacheMapWordService>('CacheWordService');
     KOREAN = '사용자';
     WORD = 'User';
@@ -44,7 +39,7 @@ describe('WordService', () => {
     });
 
     it('word가 Cach 저장소에 없고, Repository에만 있을떄 Repsitory에서 반환', async () => {
-      wordRepository.saveWord(WORD, KOREAN);
+      wordRepository.findWordByKorean.mockResolvedValue(WORD);
 
       const foundWord: string = await service.findWord(KOREAN);
 
@@ -53,6 +48,7 @@ describe('WordService', () => {
     });
 
     it('word가 Repoistory에 존재하지 않을떄 undefined 반환', async () => {
+      wordRepository.findWordByKorean.mockResolvedValue(undefined);
       const foundWord: string = await service.findWord(KOREAN);
 
       expect(foundWord).toBeUndefined();
