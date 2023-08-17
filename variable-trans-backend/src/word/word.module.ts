e9140c561db29b4c93414680fb692ad0328ccf42
Recordum <mingyu.oh15b@gmail.com>
2023-08-17 22:01:56 +0900

    [Setting] WordModule: Word typeorm 설정

--- variable-trans-backend/src/word/word.module.ts
+++ variable-trans-backend/src/word/word.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CacheMapWordService } from './cache-word/implementation/cache-map-word.service';
 import { WordServiceImpl } from './word.service.impl';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Word } from './entitiy/word.entity';
+import { CacheModule } from '@nestjs/cache-manager';
+import { redisStore } from 'cache-manager-redis-store';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Word])],
   providers: [
     {
       provide: 'CacheWordService',
