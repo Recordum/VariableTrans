8c34efe67af0ac1475f26314f171d6fe8295f3b0
Recordum <mingyu.oh15b@gmail.com>
2023-08-18 12:53:09 +0900

    [Test] TranslationService: 변수명을 컨벤션에 맞게 변환시켜주는지 테스트 추가

--- variable-trans-backend/src/translation/translation.service.spec.ts
+++ variable-trans-backend/src/translation/translation.service.spec.ts
@@ -2,12 +2,17 @@ import { Translator } from './translator/translator';
 import { TranslationService } from './translation.service';
 import { Test, TestingModule } from '@nestjs/testing';
 import { WordService } from 'src/word/word.service';
+import { VariableNameDto } from './dto/variable-name.dto';
 
 describe('translationService', () => {
   let service: TranslationService;
   let wordService: jest.Mocked<WordService>;
   let translator: jest.Mocked<Translator>;
-
+  let KOREAN;
+  let VARIABLE;
+  let SNAKE_CASE;
+  let CAMEL_CASE;
+  let PASCAL_CASE;
   beforeEach(async () => {
     wordService = {
       getVariable: jest.fn(),
@@ -26,26 +31,35 @@ describe('translationService', () => {
     }).compile();
 
     service = module.get<TranslationService>(TranslationService);
+    KOREAN = '변수명을 추천하다';
+    VARIABLE = 'recommend variable name';
+    SNAKE_CASE = 'recommend_variable_name';
+    CAMEL_CASE = 'recommendVariableName';
+    PASCAL_CASE = 'RecommendVariableName';
   });
   describe('translateVariable', () => {
-    it('wordService 에 word가 있을 시 WordService에서 반환', async () => {
-      wordService.getVariable.mockResolvedValue('hello');
+    it('wordService 에 word가 있을 시 WordService에서 네이밍 컨벤션으로 변환된 변수명 반환', async () => {
+      wordService.getVariable.mockResolvedValue(VARIABLE);
 
-      const result = await service.translateVariable('안녕');
+      const result = await service.translateVariable(KOREAN);
 
-      expect(result).toBe('hello');
+      expect(result).toEqual(
+        new VariableNameDto(SNAKE_CASE, CAMEL_CASE, PASCAL_CASE),
+      );
       expect(translator.translateVariable).not.toHaveBeenCalled();
     });
 
-    it('wordService 에 word가 없을시 translaotr에서 반환', async () => {
+    it('wordService 에 word가 없을시 translaotr에서 네이밍 컨벤션으로 변환된 변수명 반환', async () => {
       wordService.getVariable.mockResolvedValue(undefined);
-      translator.translateVariable.mockResolvedValue('hello');
+      translator.translateVariable.mockResolvedValue(VARIABLE);
 
-      const result = await service.translateVariable('안녕');
+      const result = await service.translateVariable(KOREAN);
 
-      expect(result).toBe('hello');
-      expect(wordService.getVariable).toHaveBeenCalledWith('안녕');
-      expect(translator.translateVariable).toHaveBeenCalledWith('안녕');
+      expect(result).toEqual(
+        new VariableNameDto(SNAKE_CASE, CAMEL_CASE, PASCAL_CASE),
+      );
+      expect(wordService.getVariable).toHaveBeenCalledWith(KOREAN);
+      expect(translator.translateVariable).toHaveBeenCalledWith(KOREAN);
     });
   });
 });
