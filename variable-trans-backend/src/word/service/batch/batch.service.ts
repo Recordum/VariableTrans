254316b0b1e1488d54c375a8546363b8f2272a10
Recordum <mingyu.oh15b@gmail.com>
2023-08-22 00:42:33 +0900

    [Feat] BatcService: 구현

--- variable-trans-backend/src/word/service/batch/batch.service.ts
+++ variable-trans-backend/src/word/service/batch/batch.service.ts
@@ -1,8 +1,8 @@
 import { CacheWordService } from '../cache-word/cache-word.service';
 import { WordRepository } from '../../repository/word.repository';
-import { SessionService } from '../../../user/session/session.service';
 import { Inject, Injectable } from '@nestjs/common';
 import { Word } from '../../entity/word.entity';
+import { Cron } from '@nestjs/schedule';
 
 @Injectable()
 export class BatchService {
@@ -11,12 +11,14 @@ export class BatchService {
     private readonly CacheWordService: CacheWordService,
     @Inject('WordRepository') private readonly wordRepository: WordRepository,
   ) {}
-  public async trackNewWord(word: Word) {
-    await this.CacheWordService.trackWord(word);
+
+  @Cron('0 4 * * *') // 매일 새벽 4시에 batch 실행
+  public async migrateNewWordsToRepository() {
+    const words: Word[] = await this.CacheWordService.getTrackedWords();
+    await Promise.all(words.map((word) => this.wordRepository.saveWord(word)));
   }
 
-  public async migrateNewWordsToRepository() {
-    const words: word[] = await this.CacheWordService.getTrackedWords();
-    await this.wordRepository.saveWord();
+  public async trackNewWord(korean: string) {
+    await this.CacheWordService.trackWord(korean);
   }
 }
