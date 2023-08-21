c71bc9f2d72ee4e3cee7c9af9485b4b58fcf519a
Recordum <mingyu.oh15b@gmail.com>
2023-08-21 13:52:22 +0900

    [Feat] translationController 추가

--- variable-trans-backend/src/translation/translation.controller.ts
+++ variable-trans-backend/src/translation/translation.controller.ts
@@ -1,3 +1,4 @@
+import { AuthGuard } from 'src/user/auth/auth-guard';
 import { KoreanDto } from './dto/Korean.dto';
 import { VariableNameDto } from './dto/variable-name.dto';
 import { TranslationService } from './translation.service';
@@ -5,12 +6,10 @@ import { Controller, Get, Inject, Query, UseGuards } from '@nestjs/common';
 
 @Controller('translation')
 export class TranslationController {
-  constructor(
-    @Inject('TranslationService')
-    private readonly translationService: TranslationService,
-  ) {}
+  constructor(private readonly translationService: TranslationService) {}
 
   @Get()
+  @UseGuards(AuthGuard)
   async translate(@Query() koreanDto: KoreanDto): Promise<VariableNameDto> {
     return await this.translationService.translateVariable(
       koreanDto.getKorean(),
--- variable-trans-backend/src/translation/translation.module.ts
+++ variable-trans-backend/src/translation/translation.module.ts
@@ -4,10 +4,25 @@ import { TranslationController } from './translation.controller';
 import { TranslationService } from './translation.service';
 import { PaPagoTranslator } from './translator/implementation/papago-translator';
 import { WordModule } from 'src/word/word.module';
+import { CacheModule } from '@nestjs/cache-manager';
+import * as redisStore from 'cache-manager-ioredis';
+import { RedisSessionService } from 'src/user/session/implementation/redis-sessoin.service';
 
 @Module({
-  imports: [HttpModule, WordModule],
+  imports: [
+    HttpModule,
+    WordModule,
+    CacheModule.register({
+      store: redisStore,
+      host: 'localhost',
+      port: 6379,
+    }),
+  ],
   providers: [
+    {
+      provide: 'SessionService',
+      useClass: RedisSessionService,
+    },
     {
       provide: 'Translator',
       useClass: PaPagoTranslator,
